97236e13d2a8446a638d16a88de910718926a296
Babz-G <barbaramgaynor@gmail.com>
2026-04-13 13:15:13 -0400

    suggestions displaying on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+
 function Home() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/get-all-suggestions")
+      .then((res) => res.json())
+      .then((data) => setSuggestions(data));
+  }, []);
+
   return (
     <div>
       <h1>Derp</h1>
+      {suggestions.map((suggestion) => (
+        <div key={suggestion.id}>
+          <h2>{suggestion.title}</h2>
+          <p>{suggestion.detail}</p>
+          <p>{suggestion.category}</p>
+        </div>
+      ))}
     </div>
   );
 }
