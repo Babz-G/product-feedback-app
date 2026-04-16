bc5eb66f41c6ba87be24cd43bb733b815250402c
Babz-G <barbaramgaynor@gmail.com>
2026-04-16 13:02:52 -0400

    update API URL for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://product-feedback-app-65fv.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
